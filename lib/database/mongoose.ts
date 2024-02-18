c15b25df833f14eecc2035fd46bc27801186fe8a
藤原由樹 <fujiwarayuki@fujiwarayukinoMacBook-Air.local>
2024-02-18 17:17:43 +0900

    7 commit

--- lib/database/mongoose.ts
+++ lib/database/mongoose.ts
@@ -16,11 +16,15 @@ if (!cached) {
 }
 
 export const connectToDatabase = async () => {
-  if (cached.conn) return cached.conn;
-  if (!MONGODB_URL) throw new Error("Missing MONGODB_URL")
-
-  cached.promise = cached.promise ||
-  mongoose.connect(MONGODB_URL, {dbName: "imaginary", bufferCommands: false})
-  cached.conn = await cached.promise;
-  return cached.conn;
+  try {
+    if (cached.conn) return cached.conn;
+    if (!MONGODB_URL) throw new Error("Missing MONGODB_URL")
+  
+    cached.promise = cached.promise ||
+    mongoose.connect(MONGODB_URL, {dbName: "imaginary", bufferCommands: false})
+    cached.conn = await cached.promise;
+    return cached.conn;
+  } catch (error) {
+    throw new Error("mongodb connect error")
+  }
 }
