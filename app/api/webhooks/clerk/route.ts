3fcd90950ad4e8acd401e280eef56957b92133fe
藤原由樹 <fujiwarayuki@fujiwarayukinoMacBook-Air.local>
2024-02-18 16:37:57 +0900

    6 commit

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -56,15 +56,15 @@ export async function POST(req: Request) {
 
   // CREATE
   if (eventType === "user.created") {
-    const { id, email_addresses, image_url, first_name, last_name, username } = evt.data;
+    const { id, email_addresses, username } = evt.data;
 
     const user = {
       clerkId: id,
       email: email_addresses[0].email_address,
       username: username!,
-      firstName: first_name,
-      lastName: last_name,
-      photo: image_url,
+      firstName: "",
+      lastName: "",
+      photo: "",
     };
 
     const newUser = await createUser(user);
